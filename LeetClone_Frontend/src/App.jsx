26f47b82de5e17bf6ee0835f7bdbc83a4a07fd06
j0a0cesar <jcpaduatimoteo09@gmail.com>
2025-11-05 10:01:03 -0300

    tema dark + utilitários + componentes
    correções no indes.css, main.jsx, App.jsx

--- LeetClone_Frontend/src/App.jsx
+++ LeetClone_Frontend/src/App.jsx
@@ -1,37 +1,29 @@
-import './App.css'
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import UserStat from "./pages/UserStat"; // 1. Importe a nova página
-import Navbar from "./componentes/Navbar"; // 2. Importe o Navbar
-import { useAuthContext } from './context/AuthContext';
+import UserStat from "./pages/UserStat";
+import Navbar from "./componentes/Navbar";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
   const { authUser } = useAuthContext();
 
   return (
-    // 3. Modificamos a estrutura para ter o Navbar no topo
-    <div className='h-screen flex flex-col'>
-      {/* O Navbar agora é renderizado em todas as páginas */}
-      <Navbar /> 
-      
-      {/* O conteúdo da página é renderizado abaixo do Navbar */}
-      <div className='flex-1 p-4 flex items-center justify-center bg-gray-100'>
-        <Routes>
-          {/* A Home (/) é protegida */}
-          <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
-          
-          {/* A página de estatísticas (/user-stat) também é protegida */}
-          <Route path='/user-stat' element={authUser ? <UserStat /> : <Navigate to={"/login"} />} />
+    <div className="h-screen flex flex-col bg-app text-default">
+      <Navbar />
 
-          {/* Login e SignUp só aparecem se o usuário NÃO estiver logado */}
-          <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
-          <Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+      {/* conteúdo das páginas */}
+      <div className="flex-1 p-4 flex items-center justify-center">
+        <Routes>
+          <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/user-stat" element={authUser ? <UserStat /> : <Navigate to="/login" />} />
+          <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+          <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp />} />
         </Routes>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- LeetClone_Frontend/src/main.jsx
+++ LeetClone_Frontend/src/main.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext.jsx'
-import { Toaster } from 'react-hot-toast' // Para notificações
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext.jsx';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
         <App />
-        <Toaster /> {/* Componente de notificação */}
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: '#0e0e0e',
+              color: '#e5e7eb',
+              border: '1px solid rgba(255,255,255,0.08)',
+            },
+            success: {
+              style: { background: '#0e0e0e', color: '#e5e7eb' },
+            },
+            error: {
+              style: { background: '#1a0b0b', color: '#fecaca' },
+            },
+          }}
+        />
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
